c4b4f4bb0785810abb7d0790db3752203e4e67ef
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-12-13 03:21:58 +0530

    chore: initialize axios instance

---
+++ config/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://localhost:3000',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+    },
+})
+
+export default axiosInstance;
