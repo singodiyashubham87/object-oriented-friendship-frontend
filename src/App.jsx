47ee718436c6698eef2060463191b4004187f9cf
Shubham Singodiya <singodiyashubham87@gmail.com>
2024-12-20 23:58:00 +0530

    chore: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register/index.jsx";
+import Login from "./pages/Login/index.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,31 +1,15 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 
-const Register = () => {
-  const navigate = useNavigate()
-
-  // This regex will match passwords having at least one lowercase, one uppercase character and one symbol
-  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/
-
-  const registerValidationSchema = Yup.object({
+const Login = () => {
+  const loginValidationSchema = Yup.object({
     email: Yup.string().email("❗Invalid email address").required("❗Required"),
-    password: Yup.string()
-      .min(6, "❗Password must be at least 6 characters")
-      .matches(
-        strongPasswordRegex,
-        "❗Password must contain an uppercase letter, a lowercase letter, and a symbol"
-      )
-      .required("❗Required"),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), null], "❗Passwords must match")
-      .required("❗Required"),
-    fullName: Yup.string().required("❗Required"),
+    password: Yup.string().required("❗Required"),
   });
 
   const handleSubmit = (values) => {
     console.log("Form values", values);
-    navigate('/login')
+    return
   };
 
   return (
@@ -42,10 +26,8 @@ const Register = () => {
             initialValues={{
               email: "",
               password: "",
-              confirmPassword: "",
-              fullName: "",
             }}
-            validationSchema={registerValidationSchema}
+            validationSchema={loginValidationSchema}
             onSubmit={handleSubmit}
           >
             {({ isSubmitting }) => (
@@ -76,38 +58,12 @@ const Register = () => {
                     className="text-red-500 text-sm mt-1 font-semibold"
                   />
                 </div>
-                <div>
-                  <Field
-                    name="confirmPassword"
-                    type="password"
-                    placeholder="CONFIRM PASSWORD"
-                    className="bg-transparent text-secondary-silver font-semibold text-s border-2 border-primary-silver p-2 pl-5 w-full rounded-custom-xs outline-none"
-                  />
-                  <ErrorMessage
-                    name="confirmPassword"
-                    component="div"
-                    className="text-red-500 text-sm mt-1 font-semibold"
-                  />
-                </div>
-                <div>
-                  <Field
-                    name="fullName"
-                    type="text"
-                    placeholder="FULL NAME"
-                    className="bg-transparent text-secondary-silver font-semibold text-s border-2 border-primary-silver p-2 pl-5 w-full rounded-custom-xs outline-none"
-                  />
-                  <ErrorMessage
-                    name="fullName"
-                    component="div"
-                    className="text-red-500 text-sm mt-1 font-semibold"
-                  />
-                </div>
                 <button
                   type="submit"
                   disabled={isSubmitting}
                   className="bg-primary-silver w-full text-secondary-dark text-md uppercase font-semibold py-2 rounded-custom-xs border-2 border-primary-dark hover:border-primary-silver hover:text-primary-silver hover:bg-secondary-dark cursor-pointer"
                 >
-                  Register
+                  Login
                 </button>
               </Form>
             )}
@@ -118,4 +74,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
