e0c274cf28d4c764e04784408bab4e523eac8ad0
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-01-01 01:00:58 +0530

    chore: fix layout to & add responsiveness to friends cols

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -3,9 +3,11 @@ import Navbar from './Navbar'
 
 const Layout = () => {
 	return (
-		<div className="relative flex flex-col gap-[3.125rem] py-[3.125rem] items-center justify-center min-h-screen bg-dark bg-kali bg-center bg-blend-darken">
-			<Navbar />
-			<Outlet />
+		<div className="w-full min-h-screen max-h-screen bg-dark bg-kali bg-center bg-blend-darken py-[3.125rem] flex flex-col items-center">
+			<div className="content flex-1 w-11/12 h-full max-w-5xl relative flex flex-col gap-[3.125rem] items-center justify-center overflow-auto">
+				<Navbar />
+				<Outlet />
+			</div>
 		</div>
 	)
 }
--- src/pages/Friends/index.jsx
+++ src/pages/Friends/index.jsx
@@ -63,43 +63,41 @@ const truncateName = (name) => {
 
 const Friends = () => {
 	return (
-		<div className="flex-grow flex justify-center items-center w-3/4 h-full bg-dark-glassmorphism border-2 border-secondary-silver rounded-custom-s">
-			<div className="w-full px-6 py-4">
-				<div className="flex justify-center mb-4">
-					<h2 className="text-xl text-primary-silver font-bold">Friends</h2>
-				</div>
-				<div className="grid grid-cols-5 gap-6">
-					{friendsData.map((friend, index) => (
-						<div
-							key={friend.id}
-							className="flex flex-col items-center bg-secondary-dark rounded-custom-xs p-4 shadow-lg border border-secondary-silver"
-						>
-							<img
-								src={friend.image}
-								alt={friend.name}
-								className="w-20 h-20 rounded-full object-cover mb-3 border-2 border-secondary-silver"
-							/>
-							<div className="relative group">
-								<p
-									className="text-primary-silver text-md font-semibold truncate max-w-[100px] text-center"
-									data-tooltip-id={`tooltip-${index}`}
-								>
-									{truncateName(friend.name)}
-								</p>
-								<Tooltip id={`tooltip-${index}`} place="top" effect="solid">
-									{friend.name}
-								</Tooltip>
-							</div>
-							<p className="text-secondary-silver text-sm">{friend.location}</p>
-							<button
-								type="button"
-								className="mt-3 bg-primary-silver text-secondary-dark px-4 py-2 rounded-custom-xs hover:bg-secondary-silver"
+		<div className="flex-grow flex flex-col justify-center items-center w-full h-11/12 bg-dark-glassmorphism border-xs border-secondary-silver rounded-custom-s overflow-auto px-6 py-4">
+			<div className="flex justify-center mb-4">
+				<h2 className="text-3xl text-primary-silver font-bold">Friends</h2>
+			</div>
+			<div className="w-full p-2 grid grid-cols-1 sm:grid-cols-3 gap-6 md:grid-cols-4 lg:grid-cols-5 overflow-auto">
+				{friendsData.map((friend, index) => (
+					<div
+						key={friend.id}
+						className="flex flex-col items-center bg-secondary-dark rounded-custom-xs p-4 shadow-lg "
+					>
+						<img
+							src={friend.image}
+							alt={friend.name}
+							className="w-20 h-20 rounded-full object-cover mb-3"
+						/>
+						<div className="relative group">
+							<p
+								className="text-primary-silver text-sm font-semibold truncate max-w-[100px] text-center"
+								data-tooltip-id={`tooltip-${index}`}
 							>
-								Message
-							</button>
+								{truncateName(friend.name)}
+							</p>
+							<Tooltip id={`tooltip-${index}`} place="top" effect="solid">
+								{friend.name}
+							</Tooltip>
 						</div>
-					))}
-				</div>
+						<p className="text-secondary-silver text-sm">{friend.location}</p>
+						<button
+							type="button"
+							className="mt-3 bg-primary-silver text-secondary-dark px-4 py-2 rounded-custom-xs hover:bg-secondary-silver"
+						>
+							Message
+						</button>
+					</div>
+				))}
 			</div>
 		</div>
 	)
