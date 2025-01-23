b27ee47ffc6eb4f3804e170aefef59654f27594f
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-01-24 01:00:23 +0530

    docs: update name from bookmark to bookmarks for page

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -10,7 +10,7 @@ const Bookmark = () => {
     <div className="flex-grow flex flex-col justify-evenly items-center w-full h-11/12 bg-dark-glassmorphism-30 border-xs border-secondary-silver rounded-custom-s overflow-y-auto overflow-x-hidden px-6 py-6">
       <div className="flex justify-center h-1/5">
         <h2 className="text-4xl text-primary-silver font-bold uppercase">
-          Bookmark
+          Bookmarks
         </h2>
       </div>
       <div className="w-full h-4/5 flex justify-center flex-wrap px-4 my-6 gap-6 overflow-y-auto overflow-x-hidden">
