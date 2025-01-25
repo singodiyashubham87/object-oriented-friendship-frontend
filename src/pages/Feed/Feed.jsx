af82d12930ff0dfc9e18b2032002375c331f7c04
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-01-26 01:49:04 +0530

    chore: update feed page

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,8 +1,11 @@
+import AcceptRequestIcon from "@/components/icons/AcceptRequestIcon";
+import BookmarkRequestUserIcon from "@/components/icons/BookmarkRequestUserIcon";
 import FeedNextArrowIcon from "@/components/icons/FeedNextArrowIcon";
 import FeedPrevArrowIcon from "@/components/icons/FeedPrevArrowIcon";
 import LocationIcon from "@/components/icons/LocationIcon";
-import { friendsData } from "@/pages/Friends/data/friends";
+import RejectRequestIcon from "@/components/icons/RejectRequestIcon";
 import { useState } from "react";
+import { feedData, indianStatesMap } from "./feedData";
 
 const Feed = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -10,7 +13,7 @@ const Feed = () => {
   const [nextArrowColor, setNextArrowColor] = useState("#C7C2C2");
 
   const handleNext = () => {
-    if (currentIndex < friendsData.length - 1) {
+    if (currentIndex < feedData.length - 1) {
       setCurrentIndex(currentIndex + 1);
     }
   };
@@ -45,70 +48,142 @@ const Feed = () => {
         <div className="cards w-10/12 flex justify-center items-center relative">
           {/* Left faded card */}
           {currentIndex > 0 && (
-            <div className="absolute z-1 left-16 w-[15rem] h-[19rem] border border-gray-300 bg-gray-800 opacity-50 scale-90 rounded-custom-s transition-transform p-6">
+            <div className="absolute flex flex-col gap-2 z-1 left-16 min-w-[15rem] min-h-[19rem] border border-gray-300 bg-primary-silver opacity-50 scale-90 rounded-custom-s transition-transform p-6">
               <div className="h-[10rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
                 <img
                   src={
-                    friendsData[currentIndex - 1]?.image ||
+                    feedData[currentIndex - 1]?.image ||
                     "https://media.istockphoto.com/id/1408596915/photo/employees-must-complete-the-online-survey-form-answer-the-test-questions-marked-checklist-on.jpg?s=2048x2048&w=is&k=20&c=JCkoENdhBCbi0DC0lIi1did1rHfNd8wu-AVq2bduFyw="
                   }
                   alt="Friend"
                   className="w-56 h-56 rounded-xl"
                 />
               </div>
-              <p className="text-center text-white font-bold text-lg">
-                {friendsData[currentIndex - 1]?.name}
-              </p>
-              <div className="flex justify-center items-center gap-2 mt-2">
-                <span className="text-gray-300">21, M</span>
-                <LocationIcon />
-                <span className="text-gray-300">RJ</span>
+              <div className="flex justify-between">
+                <p className="text-center text-primary-dark font-bold text-lg">
+                  {feedData[currentIndex]?.name}
+                </p>
+                <div className="flex items-center gap-1">
+                  <LocationIcon
+                    width="18"
+                    height="20"
+                    styles={{ paddingBottom: "0.2rem" }}
+                  />
+                  <p className="text-primary-dark">
+                    {indianStatesMap[feedData[currentIndex]?.state]}
+                  </p>
+                </div>
+              </div>
+              <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
+                <p className="text-primary-dark">{`${
+                  feedData[currentIndex]?.age || 21
+                }, `}</p>
+                <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+              </div>
+              <div className="buttons flex gap-8 mt-4 justify-center">
+                <div className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <RejectRequestIcon size="26" />
+                </div>
+                <div className="p-2 bg-primary-cyan hover:bg-primary-cyan-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <BookmarkRequestUserIcon size="26" />
+                </div>
+                <div className="p-2 bg-primary-green hover:bg-primary-green-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <AcceptRequestIcon size="26" />
+                </div>
               </div>
             </div>
           )}
 
           {/* Active card */}
-          <div className="z-10 w-[19rem] h-[23rem] border border-gray-400 rounded-custom-s bg-gray-800 shadow-lg p-6">
+          <div className="z-10 min-w-[19rem] min-h-[23rem] flex flex-col gap-2 border border-gray-400 rounded-custom-s bg-primary-silver shadow-lg p-6">
             <div className="w-full h-[13rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
               <img
                 src={
-                  friendsData[currentIndex]?.image ||
+                  feedData[currentIndex]?.image ||
                   "https://imgs.search.brave.com/sMcgal2XI2iAo_tND_9PgnZWVnna0tvZoWgZnUnuQDw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFtMTcteWR6TEwu/anBn"
                 }
                 alt="Friend"
                 className="w-full h-full"
               />
             </div>
-            <p className="text-center text-white font-bold text-lg">
-              {friendsData[currentIndex]?.name}
-            </p>
-            <div className="flex justify-center items-center gap-2 mt-2">
-              <span className="text-gray-300">21, M</span>
-              <LocationIcon />
-              <span className="text-gray-300">RJ</span>
+            <div className="flex justify-between">
+              <p className="text-center text-primary-dark font-bold text-lg">
+                {feedData[currentIndex]?.name}
+              </p>
+              <div className="flex items-center gap-1">
+                <LocationIcon
+                  width="18"
+                  height="20"
+                  styles={{ paddingBottom: "0.2rem" }}
+                />
+                <p className="text-primary-dark">
+                  {indianStatesMap[feedData[currentIndex]?.state]}
+                </p>
+              </div>
+            </div>
+            <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
+              <p className="text-primary-dark">{`${
+                feedData[currentIndex]?.age || 21
+              }, `}</p>
+              <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+            </div>
+            <div className="buttons flex gap-8 mt-4 justify-center">
+              <div className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                <RejectRequestIcon size="26" />
+              </div>
+              <div className="p-2 bg-primary-cyan hover:bg-primary-cyan-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                <BookmarkRequestUserIcon size="26" />
+              </div>
+              <div className="p-2 bg-primary-green hover:bg-primary-green-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                <AcceptRequestIcon size="26" />
+              </div>
             </div>
           </div>
 
           {/* Right faded card */}
-          {currentIndex < friendsData.length - 1 && (
-            <div className="absolute right-16 w-[15rem] h-[19rem] border border-gray-300 bg-gray-800 opacity-50 scale-90 rounded-custom-s transition-transform p-6">
+          {currentIndex < feedData.length - 1 && (
+            <div className="absolute flex flex-col gap-2 right-16 min-w-[15rem] min-h-[19rem] border border-gray-300 bg-primary-silver opacity-50 scale-90 rounded-custom-s transition-transform p-6">
               <div className="h-[10rem] border-2 border-primary-dark rounded-custom-xs object-cover overflow-hidden">
                 <img
                   src={
-                    friendsData[currentIndex + 1]?.image ||
+                    feedData[currentIndex + 1]?.image ||
                     "https://media.istockphoto.com/id/1434437996/photo/woman-hand-writing-on-clipboard-with-a-pen.jpg?s=2048x2048&w=is&k=20&c=r80A-T41z3wwxA-3LD_nYLF3rxMl0JrmS--dULY9TcY="
                   }
                   alt="Friend"
                   className="w-56 h-56 rounded-xl"
                 />
               </div>
-              <p className="text-center text-white font-bold text-lg">
-                {friendsData[currentIndex + 1]?.name}
-              </p>
-              <div className="flex justify-center items-center gap-2 mt-2">
-                <span className="text-gray-300">21, M</span>
-                <LocationIcon />
-                <span className="text-gray-300">RJ</span>
+              <div className="flex justify-between">
+                <p className="text-center text-primary-dark font-bold text-lg">
+                  {feedData[currentIndex]?.name}
+                </p>
+                <div className="flex items-center gap-1">
+                  <LocationIcon
+                    width="18"
+                    height="20"
+                    styles={{ paddingBottom: "0.2rem" }}
+                  />
+                  <p className="text-primary-dark">
+                    {indianStatesMap[feedData[currentIndex]?.state]}
+                  </p>
+                </div>
+              </div>
+              <div className="w-1/4 flex bg-primary-gray rounded-custom-xxs text-primary-dark border border-primary-dark font-semibold justify-center items-center gap-2">
+                <p className="text-primary-dark">{`${
+                  feedData[currentIndex]?.age || 21
+                }, `}</p>
+                <p className="text-primary-dark">{`${feedData[currentIndex]?.gender}`}</p>
+              </div>
+              <div className="buttons flex gap-8 mt-4 justify-center">
+                <div className="p-2 bg-primary-pink hover:bg-primary-pink-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <RejectRequestIcon size="26" />
+                </div>
+                <div className="p-2 bg-primary-cyan hover:bg-primary-cyan-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <BookmarkRequestUserIcon size="26" />
+                </div>
+                <div className="p-2 bg-primary-green hover:bg-primary-green-70 rounded-full border-xs border-primary-dark cursor-pointer">
+                  <AcceptRequestIcon size="26" />
+                </div>
               </div>
             </div>
           )}
@@ -118,11 +193,11 @@ const Feed = () => {
           onClick={handleNext}
           type="button"
           className={` ${
-            currentIndex === friendsData.length - 1
+            currentIndex === feedData.length - 1
               ? "opacity-50 cursor-not-allowed"
               : ""
           }`}
-          disabled={currentIndex === friendsData.length - 1}
+          disabled={currentIndex === feedData.length - 1}
           onMouseEnter={() => setNextArrowColor("#e7e7e7")}
           onMouseLeave={() => setNextArrowColor("#c7c2c2")}
         >
